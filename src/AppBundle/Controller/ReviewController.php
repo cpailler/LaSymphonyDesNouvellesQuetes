095194e6e4fe5a0f3a19eba5da487b9adcc70fee
cpailler <aprel.illectemn@gmail.com>
2018-05-27 10:51:58 +0200

    solution quête routing

---
+++ src/AppBundle/Controller/ReviewController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use AppBundle\Entity\Review;
+use AppBundle\Entity\PlaneModel;
+use AppBundle\Entity\Reservation;
+
+/**
+ * Listing controller.
+ *
+ * @Route("review")
+ */
+
+class ReviewController extends Controller
+{
+    /**
+     * Lists all review entities.
+     *
+     * @Route("/", name="review_index")
+     * @Method("GET")
+     */
+
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $reviews = $em->getRepository('AppBundle:Review')->findAll();
+
+        return $this->render('review/index.html.twig', array(
+            'reviews' => $reviews,
+        ));
+    }
+
+    /**
+     * Creates a new review entity.
+     *
+     * @Route("/new", name="review_new")
+     * @Method({"GET", "POST"})
+     */
+
+    public function newAction(Request $request)
+    {
+
+
+        return $this->render('review/new.html.twig');
+    }
+}
+
+
